0c8ad4b12c32dce5750abbf5659655a9e0a06aa7
maoxian <wjll8723@163.com>
2026-04-27 10:10:06 +0800

    增加血量开关

--- scripts/widgets/health_bar.lua
+++ scripts/widgets/health_bar.lua
@@ -2,9 +2,10 @@ local Widget = require("widgets/widget")
 local Image = require("widgets/image")
 local Text = require("widgets/text")
 
-local HealthBar = Class(Widget, function(self, target)
+local HealthBar = Class(Widget, function(self, target, show_num)
     Widget._ctor(self, "HealthBar")
     self.target = target
+    self.show_num = show_num
 
     self:SetClickable(false)
     self:SetScaleMode(SCALEMODE_PROPORTIONAL)
@@ -21,9 +22,11 @@ local HealthBar = Class(Widget, function(self, target)
     self.fill:SetPosition(0, 0)
 
     -- 文字
-    self.text = self:AddChild(Text(CHATFONT, 18))
-    self.text:SetPosition(0, 15)
-    self.text:SetScale(0.8)
+    if self.show_num then
+        self.text = self:AddChild(Text(CHATFONT, 18))
+        self.text:SetPosition(0, 15)
+        self.text:SetScale(0.8)
+    end
 
     self:Hide()
     self:StartUpdating()
@@ -41,14 +44,16 @@ function HealthBar:OnUpdate()
         return
     end
 
-    -- 1. 更新血量数值和比例 (这个可以稍微降低频率，但这里为了逻辑清晰先放这)
+    -- 1. 更新血量数值和比例
     local percent = health:GetPercent()
     self.fill:SetSize(78 * percent, 8)
     self.fill:SetPosition(-(78 * (1 - percent)) / 2, 0)
 
-    local cur = math.ceil(health:GetCurrent())
-    local max = math.ceil(health:Max())
-    self.text:SetString(string.format("%d / %d", cur, max))
+    if self.show_num and self.text then
+        local cur = math.ceil(health:GetCurrent())
+        local max = math.ceil(health:Max())
+        self.text:SetString(string.format("%d / %d", cur, max))
+    end
 
     -- 动态颜色
     if percent > 0.6 then
@@ -59,11 +64,10 @@ function HealthBar:OnUpdate()
         self.fill:SetTint(0.8, 0, 0, 0.8)
     end
 
-    -- 2. 平滑更新位置 (每帧执行)
+    -- 2. 平滑更新位置
     local x, y, z = self.target.Transform:GetWorldPosition()
     local scr_x, scr_y = TheSim:GetScreenPos(x, y + 2.5, z)
     
-    -- 简单的视口裁剪：如果坐标在屏幕外太远则隐藏
     local screen_w, screen_h = TheSim:GetScreenSize()
     if scr_x < -100 or scr_x > screen_w + 100 or scr_y < -100 or scr_y > screen_h + 100 then
         self:Hide()
