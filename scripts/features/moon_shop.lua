9643a1d1a7157c6c7bc5273137db0389662995a8
maoxian <wjll8723@163.com>
2026-06-29 23:12:47 +0800

    feat: 增加小月亮图标

--- scripts/features/moon_shop.lua
+++ scripts/features/moon_shop.lua
@@ -15,10 +15,12 @@ end
 -- 注册商店标签（modinit 时 AddRecipeFilter 才可用）
 local has_filter = false
 if AddRecipeFilter ~= nil then
+    local tex = "lunar_seed.tex"
+    local atlas = GetInventoryItemAtlas(tex) or "images/inventoryimages.xml"
     AddRecipeFilter({
         name = "MOON_SHOP",
-        atlas = "images/inventoryimages.xml",
-        image = "goldnugget.tex",
+        atlas = atlas,
+        image = tex,
     })
     has_filter = true
     print("[小月亮商店] 独立标签注册成功")
